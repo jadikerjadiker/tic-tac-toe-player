eb7fc56589ab9fae6caf1697ec7dc2c69290cc33
jadiker <happylondon4@gmail.com>
2016-12-19 23:14:55 +0000

    cleaned up neural net backprop code

--- main/NeuralNet.py
+++ main/NeuralNet.py
@@ -4,7 +4,16 @@ import math #for e^x (math.exp())
 #This thing causes some errors to pop up on the side of cloud9, you can just ignore them.  (This works, although I don't understand how or why at this point...)
 @np.vectorize #turns it into a function that can be applied element-wise on a column vector
 def sigmoid(num):
-    return 1/(1+math.exp(-num))
+    try:
+        return 1/(1+math.exp(-num))
+    except OverflowError:
+        if num<100:
+            return 0
+        elif num>100:
+            return 1
+        else:
+            print("Here's the number that caused the error:{}".format(num))
+            raise
 
 def resizeToVector(thing):
     return np.resize(thing, (thing.size, 1))
@@ -37,7 +46,7 @@ class NeuralNet:
         def insertBias(inpt):
             return np.insert(inpt, [0], [[1]], axis = 0)
             
-        assert len(inpt)==self.architecture[0], "Input must have the correct number of values. (In this case, {} values.)".format(self.architecture[0])
+        assert len(inpt)==self.architecture[0], "Input\n{}\nmust have the correct number of values. (In this case, {} values.)".format(inpt, self.architecture[0])
         inpt = makeVector(inpt)
         activations = [inpt]
         for weightMatrix in self.net:
@@ -57,65 +66,60 @@ class NeuralNet:
     
     #back propagation
     #batch is a list of tuples [(vector-like input, vector-like solution), (vector-like input, vector-like solution), ...]
+    #TODO clean up and comment clearly
+    #TODO use normal expressions like '-' and '+' when possible
     def backProp(self, batch, normalization = 1, runTime = 1):
+        #The list containing the matricies of derivatives. At the end we subtract these matricies from the weight matricies
+        #So you can also think of this as the list of matricies of how much the weights need to change
+        #delta[l-1] is the delta matrix that matches with the weight matrix that goes from layer l to layer l+1
         delta = [np.zeros((self.architecture[layerIndex], self.architecture[layerIndex-1]+1)) for layerIndex in range(1, len(self.architecture))]
         for iteration in range(runTime):
             for trainingExample in batch:
+                #list of errors in each layer. errors[l-1] is the error of layer l
                 errors = []
-                #a is a list of activation vectors, the first vector is the one from the second layer.
-                #print("trainingExample:\n{}".format(trainingExample))
+                #activations is the list of activation vectors, the first vector is the one from the input layer (which can be negative and greate than 1).
                 activations = self.getActivations(trainingExample[0])
-                #print("architecture:\n{}".format(self.architecture))
-                #print("Getting activations finished.")
-                #print("Net:\n{}".format(self))
+                #make the solution a vector
                 y = makeVector(trainingExample[1])
-                #calculate the error in the last layer
-                lastLayerError = activations[-1]-y
-                error = np.array(lastLayerError)
-                errors.append(error)
+                #check to make sure solution vector has the right size
+                assert activations[-1].shape==y.shape, "Target vector (solution)\n{}\nmust have the correct number of values. (In this case, {} values.)".format(y, self.architecture[-1])
+                #calculate the error in the last layer and add it to the list of errors
+                errors.append(activations[-1]-y)
+                
                 #go backwards through each layer of the net and calculate the error for those
                 #i = (number of layer we're computing the error for) - 1
                 #i is basically just converting from computer to human numbers
                 #starts at the second to last layer and goes all the way to the input layer
-                #(even though it doesn't need to calculate the error of the input layer)
+                #(even though it doesn't really need to calculate the error of the input layer)
                 #remember, here, we ignore the bias values in the weight matrix
                 for i in range(len(self.architecture)-2, -1, -1):
-                    #print(i)
-                    #print("activations:\n{}".format(activations))
-                    #get the activation value of the layer I'm computing the error for
+                    #get the activation values of the layer whose error is being computed
                     activation = activations[i]
+                    
                     #we have the activation value of our layer
                     #and the error of the layer in front of us
-                    #so we can compute the derivative and add it to delta
-                    #print("error:\n{}".format(error))
-                    #print("activation:\n{}".format(activation))
+                    #so now we can compute the derivative
+                    #and add it to delta
+                    
+                    #Compute the delta, following the formula
                     deltaChangeWithoutBias = np.matmul(error, np.transpose(activation))
-                    #print("deltaChangeWithoutBias:\n{}".format(deltaChangeWithoutBias))
+                    #insert in the delta values for the bias (which is just the error of the layer) as the first column in the matrix
                     deltaChange = np.insert(deltaChangeWithoutBias, 0, error.flatten(), axis = 1)
-                    #print("deltaChange:\n{}".format(deltaChange))
+                    #add this deltaChange to the total delta (this is what makes it batch training; the mini deltas are added up)
                     delta[i] = np.add(delta[i], deltaChange)
-                    #print("weightMatrix:\n{}".format(self.net[i]))
-                    #take out the bias from the weight matrix going from this layer to the next layer
+                    
+                    
+                    #take out the bias from the weight matrix going from this layer to the next layer so we can compute the error
                     noBiasWeightMatrix = np.delete(self.net[i], 0, 1)
-                    #print("activation:\n{}".format(activation))
-                    #print("noBiasWeightMatrix:\n{}".format(noBiasWeightMatrix))
-                    #print("error:\n{}".format(error))
-                    #step1 = np.matmul(np.transpose(noBiasWeightMatrix), error) #should give n(l) x 1
-                    #print("activationDerivative:\n{}".format(activationDerivative))
-                    #print("activation:\n{}".format(activation))
-                    #step2 = np.multiply(activationDerivative, activation)
-                    #follow the formula to compute the error of the layer
+                    #follow the formula to compute the error of the next layer back
                     error = np.multiply(np.multiply(np.matmul(np.transpose(noBiasWeightMatrix), error), activation), np.subtract(np.ones(activation.shape), activation))
-                    #print("Error:\n{}".format(error))
+                    
+                    #add this error to the start of errors
                     errors.insert(0, error)
-                    #weightTranspose = np.transpose()
-                
+
                 #update the weights
                 for matrixIndex in range(len(self.net)):
                     self.net[matrixIndex] = np.subtract(self.net[matrixIndex], delta[matrixIndex])
-                print("Iteration {}".format(iteration+1))
-                print("All errors:\n{}".format(errors))
-                print("Final gradient amount:\n{}".format(delta))
     
     def __str__(self):
         ans = "Weights:\n"
@@ -165,14 +169,14 @@ if __name__ == "__main__":
     n.backProp([([1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1])])
     '''
 
-    training = ([1, 2, 3], [1, 0])
-    n = NeuralNet([3, 4, 3, 2])
+    training = ([1, 0, 1, 1, 1, 0, 1, 1, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0])
+    n = NeuralNet([9, 9, 9, 9])
     w = n.net[0]
     import copy
     oldMatrix = copy.deepcopy(n.net)
     beforeResponse = n.run(training[0])
     beforeError = n.getErrorOfLastLayer(*training)
-    n.backProp([training], runTime = 400)
+    n.backProp([training], runTime = 1)
     print("beforeResponse:\n{}\nnewResponse:\n{}\nbeforeError:\n{}\nnewError:\n{}".format(beforeResponse, n.run(training[0]), beforeError, n.getErrorOfLastLayer(*training)))
     #print(oldMatrix)
     #print(n.net)
