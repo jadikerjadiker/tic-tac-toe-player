19c5d7d8b479d3f32739d815d8007df4573ac57f
jadiker <happylondon4@gmail.com>
2017-01-08 00:13:32 +0000

    useful.printPercent() updated

--- main/UsefulThings.py
+++ main/UsefulThings.py
@@ -19,8 +19,14 @@ def askYesOrNo(question):
 
 def assertPython3():   
     assert sys.version_info[0] >= 3, "Python version needs to be at least 3"
-  
-def printPercent(indexNumber, outOf, logAmt = 1, incrementAmt = 1, roundAmt = 1):  
+
+#If you're running a for loop like
+#'for i in range(maxVal)'
+#Then in the loop you can run printPercent(i, maxVal)
+#...to print the percentage that the loop is done
+#This doens't work perfectly because of rounding error, but it's close enough.
+#(Sometimes it will skip or double print a number)
+def printPercent(indexNumber, outOf, incrementAmt = 1, roundAmt = 1):  
     printPc = 100.0*(indexNumber+1)/outOf
-    if printPc%logAmt<incrementAmt:
+    if printPc%logAmt<(100.0/outOf):
         print("{}%...".format(round(printPc, roundAmt)))
